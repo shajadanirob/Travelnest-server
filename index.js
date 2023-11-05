393056238fd9f95d4e51906c7a2fe3119415a1b7
hossainnirob <shajadanirob@gmail.com>
2023-11-05 11:30:30 +0600

    create a git ignore file and create get service and create a post services

--- index.js
+++ index.js
@@ -1,9 +1,76 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // middleWare
 app.use(cors())
 app.use(express.json())
+
+
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ul0jqdv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const servicesCollection = client.db('travelnest').collection('services')
+
+     // post services 
+     app.post('/services',async(req ,res) =>{
+        
+     })
+
+    // services get
+    app.get('/services',async (req,res) =>{
+        const cursor = servicesCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+   
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("TravelNest server is running");
+});
+app.listen(port, () => {
+    console.log(`TravelNest server is running port ${port}`);
+});
